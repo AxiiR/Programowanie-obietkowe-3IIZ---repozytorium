447548ec1887affe1568c659d4cbba30987c94ac
AxiiR <AxiiiR7@gmail.com>
2025-11-13 20:56:31 +0100

    Lab1, Lab2
    Wykonanie laboratorium 1 i 2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ class Program
     static void Main(string[] args)
     {
         Class1.Zadanie1();
-        Class2.Zadania2();
+        Class1.Zadanie2();
+        Class1.Zadanie3();
+        Class1.Zadanie4();
+        Class1.Zadanie5();
+        Class1.Zadanie6();
+        Class1.Zadanie7();
+        Class1.Zadanie8();
+        Class1.Zadanie9();
+        Class1.Zadanie10();
     }
 }
